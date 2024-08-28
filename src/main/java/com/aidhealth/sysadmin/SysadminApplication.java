bc1bef393c4bd13823d5e1738a09a93c291ea1ba
jeef-js <jeffersonthiago.ga@gmail.com>
2024-08-27 23:16:15 -0300

    feat: first commit

---
+++ src/main/java/com/aidhealth/sysadmin/SysadminApplication.java
@@ -0,0 +1,13 @@
+package com.aidhealth.sysadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aidhealth/sysadmin/SysadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidhealth.sysadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
